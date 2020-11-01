fb42e21543a5275961ecc3a76176b56b1f134fd5
Ryujeuk <jeuk817@gmail.com>
2020-11-01 19:01:23 +0900

    feat: FrontMemberController 에 주석 추가

--- src/kr/or/bit/controller/FrontMemberController.java
+++ src/kr/or/bit/controller/FrontMemberController.java
@@ -42,49 +42,49 @@ public class FrontMemberController extends HttpServlet {
     	Action action=null;
     	ActionForward forward=null;
     	
-    	if(urlCommand.equals("/Main.do")) { // Get Main page
+    	if(urlCommand.equals("/Main.do")) { // 메인 페이지
     		action = new MainService();
     		forward = action.execute(request, response);
-    	} else if(urlCommand.equals("/JoinForm.do")) {
+    	} else if(urlCommand.equals("/JoinForm.do")) { // 회원가입 페이지
     		action = new JoinFormService();
     		forward = action.execute(request, response);
-    	} else if(urlCommand.equals("/Login.do")) {
-    		action = new LoginService();
-    		forward = action.execute(request, response);
-    	} else if(urlCommand.equals("/Logout.do")) {
-	    	action = new LogoutService();
-	    	forward = action.execute(request, response);
-    	} else if(urlCommand.equals("/MemberList.do")) {
-	    	action = new MemberListService();
-	    	forward = action.execute(request, response);
-    	} else if(urlCommand.equals("/MemberSearch.do")) {
-	    	action = new MemberSearchService();
-	    	forward = action.execute(request, response);
-    	} else if(urlCommand.equals("/MemberDelete.do")) {
-	    	action = new MemberDeleteService();
-	    	forward = action.execute(request, response);
-    	} else if(urlCommand.equals("/SignUp.do")) {
+    	} else if(urlCommand.equals("/SignUp.do")) { // 회원가입
 	    	action = new SignUpService();
 	    	forward = action.execute(request, response);
-    	} else if(urlCommand.equals("/MemberDetail.do")) {
+    	} else if(urlCommand.equals("/Login.do")) { // 로그인
+    		action = new LoginService();
+    		forward = action.execute(request, response);
+    	} else if(urlCommand.equals("/Logout.do")) { // 로그아웃
+	    	action = new LogoutService();
+	    	forward = action.execute(request, response);
+    	} else if(urlCommand.equals("/MemberList.do")) { // 회원관리 페이지
+	    	action = new MemberListService();
+	    	forward = action.execute(request, response);
+    	} else if(urlCommand.equals("/MemberSearch.do")) { // 회원이름으로 검색
+	    	action = new MemberSearchService();
+	    	forward = action.execute(request, response);
+    	} else if(urlCommand.equals("/MemberDelete.do")) { // 회원 삭제
+	    	action = new MemberDeleteService();
+	    	forward = action.execute(request, response);
+    	} else if(urlCommand.equals("/MemberDetail.do")) { // 회원 상세정보 페이지
 	    	action = new MemberDetailService();
 	    	forward = action.execute(request, response);
-    	} else if(urlCommand.equals("/MemberEditForm.do")) {
+    	} else if(urlCommand.equals("/MemberEditForm.do")) { // 회원 수정 페이지
 	    	action = new MemberEditFormService();
 	    	forward = action.execute(request, response);
-    	} else if(urlCommand.equals("/MemberEdit.do")) {
+    	} else if(urlCommand.equals("/MemberEdit.do")) { // 회원 수정
 	    	action = new MemberEditService();
 	    	forward = action.execute(request, response);
     	}
     	
     	if(forward != null) {
-    		if(forward.isRedirect()) { //true 
+    		if(forward.isRedirect()) {
     			response.sendRedirect(forward.getPath());
-    		}else { //false (모든 자원 ) 사용
+    		}else {
     			RequestDispatcher dis  = request.getRequestDispatcher(forward.getPath());
     			dis.forward(request, response);
     		}
-    	} else {
+    	} else { // 404
     		RequestDispatcher dis  = request.getRequestDispatcher("/WEB-INF/views/Error404.jsp");
     		dis.forward(request, response);
     	}
